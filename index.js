efbfe01c71c2265c31b0b2dc3fc017b1b7e57c8e
rodrigo laime <rodrigolaime141@gmail.com>
2023-04-11 15:16:10 -0300

    failed cors publick

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(/* {
+/* app.use(cors({
     origin: ['http://localhost:3000'],
     credentials: true,
-} */));
+})); */
+app.use(cors());
 dotenv.config();
 
 // connection router

04a50c28cdd5a3790d0c75dfc7737162906a9f19
rodrigo laime <rodrigolaime141@gmail.com>
2023-04-13 18:00:51 -0300

    styles front

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-/* const corsOption = {
+const corsOption = {
   origin: [
     'http://localhost:3000',
     'https://main--gentle-taffy-cb9058.netlify.app'
   ],
   credentials: true,
 }
-app.use(cors(corsOption)); */
-app.use(cors()); // da acceso a cualquier origin
+app.use(cors(corsOption));
+/* app.use(cors()); // da acceso a cualquier origin */
 dotenv.config();
 
 // connection router
